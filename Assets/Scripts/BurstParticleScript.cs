4e15c66a20ba4b0b4a6573b23f23ccb0c189e5b1
Simon Katzer <skatzer@ucsc.edu>
2019-06-03 22:41:05 -0700

    Added a particle system which is supposed to react to sound. I'm still fiddling with it.

---
+++ Assets/Scripts/BurstParticleScript.cs
@@ -0,0 +1,55 @@
+﻿// Based off the script from David's Week 8 Section
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurstParticleScript: MonoBehaviour {
+
+    ParticleSystem ps;
+    public float threshold = 0.4f;
+
+    void Start () 
+    {
+	    // Get Particle system component
+	    ps = GetComponent<ParticleSystem>();
+        // Call particle play function
+	}
+
+	void Update () {
+    
+		int numPartitions = 1;
+		float[] aveMag = new float[numPartitions];
+		float partitionIndx = 0;
+		int numDisplayedBins = 512 / 2; 
+
+		for (int i = 0; i < numDisplayedBins; i++) 
+		{
+			if(i < numDisplayedBins * (partitionIndx + 1) / numPartitions){
+				aveMag[(int)partitionIndx] += AudioPeer.spectrumData [i] / (512/numPartitions);
+			}
+			else{
+				partitionIndx++;
+				i--;
+			}
+		}
+
+		for(int i = 0; i < numPartitions; i++)
+		{
+			aveMag[i] = (float)0.5 + aveMag[i]*100;
+			if (aveMag[i] > 100) {
+				aveMag[i] = 100;
+			}
+		}
+
+		float mag = aveMag[0];
+		
+        // if mag is greater than some threshold(0.6)
+        // emit particle using emit function
+        if(mag > threshold){
+        	ps.Emit(1);
+        }
+
+	}
+
+
+}
